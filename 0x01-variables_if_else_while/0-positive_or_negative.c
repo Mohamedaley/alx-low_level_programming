a5d3ea6b02013074bee61e288d78d52967f62296
vulturepremier <vulturepremier@gmail.com>
2023-12-17 06:16:29 -0800

    My first commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/*
+ * main - entry point of our program
+ * return : always 0 (Success)
+ */
 int main(void)
 {
 	int n;
