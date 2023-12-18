92ce1a811e0656fd3523739a83151188a518d04c
vulturepremier <vulturepremier@gmail.com>
2023-12-17 17:13:16 -0800

    My first commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i = 'a';
+	char j = 'z'
+
+	while (i != 'e' && i != 'q' && i <= z)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
