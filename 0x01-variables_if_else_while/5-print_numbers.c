2c45ca297a1923172756227c5826950f8c134815
vulturepremier <vulturepremier@gmail.com>
2023-12-17 17:20:30 -0800

    My first commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * return: always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+	putchar(i);
+	}
+	puthcar('\n');
+	return (0);
+}
