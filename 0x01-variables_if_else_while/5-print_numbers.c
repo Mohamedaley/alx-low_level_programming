d763455340fda2737882adf043ff9aeba0349099
vulturepremier <vulturepremier@gmail.com>
2023-12-17 17:26:24 -0800

    My first commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * return: Always 0 (Success)
+ * main - prints all single digit numbers of base
+ * 10 starting from 0, followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i = 0;
+	int n;
 
-	for (i = 0; i <= 9; i++)
-	{
-	printf("%d", i);
-	}
-	putchar('\n');
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
 	return (0);
 }
