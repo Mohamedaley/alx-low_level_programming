d1a79e1c653216c90bb27c9f4e474bec914c1def
vulturepremier <vulturepremier@gmail.com>
2023-12-20 18:03:34 -0800

    My first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+	putchar('i');
+	i++;
+	}
+	putchar('\n');
+}
