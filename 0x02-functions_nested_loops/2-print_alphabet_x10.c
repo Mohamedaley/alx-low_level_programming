5aa70dda088f1c80e2363fa31d391d8cdf2544f5
vulturepremier <vulturepremier@gmail.com>
2023-12-20 18:20:54 -0800

    My first commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * void print_alphabet_x10 - Entry point
+ *
+ * Return:0
+ */
+
+void print_alphabet_x10(void);
+{
+	int i;
+	char c = 'a';
+
+	for(i = 0; i <= 9; i++)
+	{
+		while(c <= z)
+		{
+			_putchar(c);
+			c++;
+		}
+	}		_putchar('\n');
+}
