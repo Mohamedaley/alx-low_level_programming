bfcf2f66d3e7548f90dd86630c61a80b6f6afce7
vulturepremier <vulturepremier@gmail.com>
2023-12-20 18:52:05 -0800

    My first commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * Return: 0 or 1
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
