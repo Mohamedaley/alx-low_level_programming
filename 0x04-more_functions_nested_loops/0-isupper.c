db2f4d93dd661102b483c3096a5854692b10f7e3
vulturepremier <vulturepremier@gmail.com>
2023-12-22 04:28:25 -0800

    My first commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 
 /**
  * _isupper - is my own fucntion that :checks for uppercase character
- *
+ * * @c: this the character which we want to check
  * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
