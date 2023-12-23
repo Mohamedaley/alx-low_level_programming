dd85e0202a581ec80b905659fa3ea2fbc7a42b14
vulturepremier <vulturepremier@gmail.com>
2023-12-23 04:37:55 -0800

    My first commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int i=0,j;
+
+	while (i < 24)
+	{
+	j=0;
+	while ( j < 60)
+	{
+		_putchar('0' + i / 10);
+		_putchar('0' + i % 10);
+		_putchar(':');
+		_putchar('0' + j / 10);
+		_putchar('0' + j % 10);
+		_putchar('\n');
+		j++;
+	}
+	i++;
+	}
+}
