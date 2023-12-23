66bed3280d48b18d97917afc28b17cf69fba1cfc
vulturepremier <vulturepremier@gmail.com>
2023-12-23 03:35:41 -0800

    My first commmit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,10 +2,13 @@
 /**
  * print_last_digit - prints the last digit of a number
  * @n: the int to extract the last digit from
+ * @r: is the variable which i store the result;
  * Return: value of the last digit
  */
 int print_last_digit(int n)
 {
-return (n % 10);
-return (n % 10);
+	r = (n % 10);
+	putchar(r+'0');
+	return(r);
+
 }
