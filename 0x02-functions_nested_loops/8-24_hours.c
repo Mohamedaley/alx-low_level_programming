f14fb59664450b947636ff6a487edea173561ce6
vulturepremier <vulturepremier@gmail.com>
2023-12-23 04:40:35 -0800

    My first commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,12 +5,12 @@
  */
 void jack_bauer(void)
 {
-	int i=0,j;
+	int i = 0, j;
 
 	while (i < 24)
 	{
-	j=0;
-	while ( j < 60)
+	j = 0;
+	while (j < 60)
 	{
 		_putchar('0' + i / 10);
 		_putchar('0' + i % 10);
