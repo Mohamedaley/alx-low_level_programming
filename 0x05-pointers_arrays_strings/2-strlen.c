6ac5d9277df5b74e205b3a62c8423335da356798
vulturepremier <vulturepremier@gmail.com>
2024-01-02 03:43:07 -0800

    My first commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strlen - function giving the length of string;
+ * @s: string address;
+ */
+int _strlen(char *s)
+{
+	int len = sizeof(s) / sizeof(char);
+
+	return (len);
+}
