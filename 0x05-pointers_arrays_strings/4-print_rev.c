4a23d1524b140777c6e94d19f9bb29db0c3cdcb7
vulturepremier <vulturepremier@gmail.com>
2024-01-02 23:55:02 +0200

    My first commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * rev_string - Function to print the string in reverse;
+ * print_rev - Function to print the string in reverse;
  * @s: this is the string;
  * Return: Always 0 (Success)
  */
 void print_rev(char *s)
 {
-	int i;;
+	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
