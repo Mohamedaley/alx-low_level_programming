c85b618540ec09536130effb29a6a9dbd206d7fd
vulturepremier <vulturepremier@gmail.com>
2024-01-03 00:18:12 +0200

    My first commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * rev_string - function which reverse whole string;
+ * @s: contains the string
+ * Return: Alwasy 0 (Success);
+ */
+void rev_string(char *s)
+{
+
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - prints the every character of the string
+ * @str: stores the string
+ * Return: Always 0 (Success)
+ */
+void puts2(char *str)
+{
+	int i, j;
+
+	while (str[i] != '\0')
+	{
+		i++
+	}
+	for (j = i; j >= 0; j--)
+	{
+	_putchar(str[j]);
+	_putchar('\n');
+	}
+}
