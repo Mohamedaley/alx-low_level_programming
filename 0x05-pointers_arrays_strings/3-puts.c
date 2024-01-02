44ac39cad9fb9adf79f554259189ede0dea37fc1
vulturepremier <vulturepremier@gmail.com>
2024-01-02 23:07:52 +0200

    My first commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - Function was made to print string on the screen (Standard output);
+ * @str: is the string which we receive;
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	printf("%s", str);
+}
