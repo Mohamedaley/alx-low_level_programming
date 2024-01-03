acbbff49881e6b7d453f9755d9498183d4a2fc9f
vulturepremier <vulturepremier@gmail.com>
2024-01-03 13:39:03 +0200

    My first commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,11 +9,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i <= n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
-		if(i == 0)
+		if (i == 0)
 		{
-		printf("%d ,", a[i]);
+		printf("%d", a[i]);
 		}
 	printf(", %d", a[i]);
 	}
