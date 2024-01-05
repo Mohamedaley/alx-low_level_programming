b9989288dda13958c85a35f055b45d4e3fb9aeec
vulturepremier <vulturepremier@gmail.com>
2024-01-05 15:56:33 +0200

    My first commit

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,9 +11,9 @@ char *string_toupper(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if ( s[i] >= 'a' && s[i] <= 'z')
+		if (s[i] >= 'a' && s[i] <= 'z')
 		{
-			s[i] = s[i] -32;
+			s[i] = s[i] - 32;
 		}
 		return (s);
 }
