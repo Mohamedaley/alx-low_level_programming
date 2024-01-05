5da4bf9de6dc2ccf002df8371658e2dd1204d500
vulturepremier <vulturepremier@gmail.com>
2024-01-05 12:23:06 +0200

    My first commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncat - a modified function to concatenate two strings
+ * @dest: represents the first string
+ * @src: represents the second string
+ * @n: represents the number of letters which we want to concatenate;
+ * Return: function is designed to return a pointer to the destination/string;
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j <= n; j++)
+	{
+	dest[i] = src[j];
+	i++;
+	}
+	dest[i] = '\0';
+}
