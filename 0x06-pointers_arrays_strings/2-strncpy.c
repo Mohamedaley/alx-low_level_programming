1d5382f9406ef43c8256eb863d6a45efd47d0258
vulturepremier <vulturepremier@gmail.com>
2024-01-05 13:21:52 +0200

    My first commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,17 +9,18 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0, j, x = 0, y = 0;
+	int i = 0, j = 0, x = 0, y = 0;
 
 	while (src[i] != 0 && i <= n)
 	{
 		i++;
 	}
-	for (j = 1; j <= i; j++)
+	while (j < i)
 	{
 		dest[x] = src[y];
 		x++;
 		y++;
+		j++;
 	}
 	x++;
 	dest[x] = '\0';
