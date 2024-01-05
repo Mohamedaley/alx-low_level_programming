0faf5593259939cd35aeab74b2ff97e3c0ef6200
vulturepremier <vulturepremier@gmail.com>
2024-01-05 18:24:18 +0200

    My first commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,20 +9,20 @@
 char *leet(char *s)
 {
 	int i, j;
+
 	char *a = "aAeEoOtTlL";
 	char *b = "4433007711";
 
-	for (i = 0; s[i] != 0 ;i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
 			if (s[i] == a[j])
 			{
-
-				s[i] == b[j];
-
+				s[i] = b[j];
 			}
-
 		}
 	}
+
+	return (s);
 }
