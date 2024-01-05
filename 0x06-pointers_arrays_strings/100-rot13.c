6fd5aeb1e9c7773583a81f8930b9fbcdc44587b2
vulturepremier <vulturepremier@gmail.com>
2024-01-05 19:17:22 +0200

    My first commit

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,31 +1,29 @@
 #include "main.h"
+
 /**
- * rot13 - this function is made to encode a text;
- * @s: this is the text;
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
+ *
+ * Return: the resulting string
  */
 char *rot13(char *s)
 {
-	char *enc = "NOPQRSTUVWXYZABCDEFGHIJKLM";
-	char *encl = "nopqrstuvwxyzabcdefghijklm";
-	char *alphl = "abcdefghijklmnopqrstuvwxyz";
-	char *alphu = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int i,j;
+	int i, j;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j <= 25; j++)
-
+		for (j = 0; a[j] != '\0'; j++)
 		{
-		if (s[i] == alphl[j])
-
+			if (s[i] == a[j])
 			{
-				s[i] = encl[j];	
-			}
-		else if(s[i] == alphu)
-
-			{
-			s[i] = enc[j]
+				s[i] = b[j];
+				break;
 			}
 		}
 	}
+
+	return (s);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -8,3 +11,7 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+#endif
