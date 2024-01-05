ac0eeeb73eb0a661afe19945244a5a90d1f7a658
vulturepremier <vulturepremier@gmail.com>
2024-01-05 12:27:10 +0200

    My first commit

--- 0x06-pointers_arrays_strings/test.c
+++ 0x06-pointers_arrays_strings/test.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * _strncat - a modified function to concatenate two strings
  * @dest: represents the first string
@@ -8,17 +8,16 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, j = 0;
+        int i = 0, j = 0;
 
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	for (j = 0; j <= n; j++)
-	{
-	dest[i] = src[j];
-	i++;
-	}
-	dest[i] = '\0';
-	return (destination);
+        while (dest[i] != '\0')
+        {
+                i++;
+        }
+        for (j = 0; j <= n; j++)
+        {
+        dest[i] = src[j];
+        i++;
+        }
+        dest[i] = '\0';
 }
