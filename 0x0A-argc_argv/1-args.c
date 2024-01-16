9d284ed7b9da37781ca0e81971d01e77052a4587
vulturepremier <vulturepremier@gmail.com>
2024-01-16 23:24:55 +0200

    My first commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - function to print the number of passed arguements
+* @argc: the number of passed arguements
+* @argv: the passed arguements
+* Return: always 0
+*/
+int main(int argc, char *argv[])
+{
+	putchar(argc);
+	return (0);
+}
