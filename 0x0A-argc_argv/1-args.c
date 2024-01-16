4c44f4a133c581a60ae93c8d3c265370e170fad5
vulturepremier <vulturepremier@gmail.com>
2024-01-16 23:26:20 +0200

    My first commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
-* main - function to print the number of passed arguements
-* @argc: the number of passed arguements
-* @argv: the passed arguements
-* Return: always 0
-*/
-int main(int argc, char *argv[])
+ * main - print the number of arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	putchar(argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
