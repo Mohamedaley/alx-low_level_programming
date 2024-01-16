76673579c7d9799496aeff1b1d44393b94a14721
vulturepremier <vulturepremier@gmail.com>
2024-01-16 23:14:49 +0200

    My first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function to print command line arguement
+ * @argc: number of command line arguements
+ * @argv: the value
+ * Return: Always 0 Success
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s", argv);
+	}
+	return (0);
+}
