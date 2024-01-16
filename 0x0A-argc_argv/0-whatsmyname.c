9c704fd44d16e1ddb788499c76ee7a18fa607f22
vulturepremier <vulturepremier@gmail.com>
2024-01-16 23:19:49 +0200

    My first commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+
 /**
- * main - function to print command line arguement
- * @argc: number of command line arguements
- * @argv: the value
- * Return: Always 0 Success
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int i = 0;
+	printf("%s\n", *argv);
 
-	for (i = 0; i <= argc; i++)
-	{
-		printf("%s", argv);
-	}
 	return (0);
 }
