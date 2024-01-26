8a7616a1d7ba33b9f1d5a810c6d9235e963e0fc7
vulturepremier <vulturepremier@gmail.com>
2024-01-26 19:43:47 +0200

    My first commit

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,19 @@
+#include <variadic_functions.h>
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	int i = n;
+	va_list ptr;
+
+	va_start(ptr, n)
+	if (!n)
+	{
+	printf("\n");
+	return;
+	}
+	while (i--)
+	{
+	printf("%s%s", va_arg(ptr, char) ? va_arg(ptr,char) : "nil", separator ? separator : "");
+	}
+	putchar ('\n');	
+	va_end(ptr);
+}
