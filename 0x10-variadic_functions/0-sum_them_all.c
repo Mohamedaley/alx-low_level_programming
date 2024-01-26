2f8706ef7f44180781c5b876cc4473cc2f9a6608
vulturepremier <vulturepremier@gmail.com>
2024-01-26 12:01:24 +0200

    My fist commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+ * sum_them_all - a function to sum all of the parameter passed
+ * @n: the number of our paratmeters;
+ * Return: the summation;
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0, i = 0;
+
+	if (n == 0)
+		exit(0);
+	va_list ptr;
+
+	va_start(ptr, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ptr, int);
+	}
+	va_end(ptr);
+	return (sum);
+}
