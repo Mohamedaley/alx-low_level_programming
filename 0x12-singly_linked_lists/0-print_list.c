e352d4bf0049ca73e2b33f8491d17b90a1b451a9
vulturepremier <vulturepremier@gmail.com>
2024-02-01 17:42:18 +0200

    My fist commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_list - function to print sinlgle linked list
+ * @h: the pointer passed
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+	size_t len = 0;
+	list_t *head;
+
+	head = h;
+	if (head == NULL)
+		return(0);
+	while (head != NULL)
+	{
+		if (head->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("%s", head->str);
+			printf("%d", head->len);
+		}
+		head = head->next;
+		count++;
+	}
+	len = count;
+	return (len);
+}
