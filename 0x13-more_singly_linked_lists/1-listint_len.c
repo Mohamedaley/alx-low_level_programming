fc34703e71fe7b08e5e49d704bed36903e6ce0d8
vulturepremier <vulturepremier@gmail.com>
2024-02-05 21:44:34 +0200

    My first commit

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * listint_len - function to declear the length of linked list;
+ * @h: the head of our list;
+ * Return: the number of elements of the list;
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t len = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
