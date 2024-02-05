9c30b5faaa8b601771d58e714d044c486743c85f
vulturepremier <vulturepremier@gmail.com>
2024-02-05 21:40:40 +0200

    My first commit

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * this is our needed libraries
- */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
@@ -16,8 +14,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
 #endif
