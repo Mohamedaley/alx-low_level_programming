0aeb0e91f819fa3cb120ddb914320a7fd4b48721
vulturepremier <vulturepremier@gmail.com>
2024-02-06 19:52:07 +0200

    My first commit

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,9 +12,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (head == NULL)
 		return (NULL);
-
 	ptr = head;
-	for (i = 0; i < n; i++)
+	for (i = 0; i < index; i++)
 	{
 		ptr = ptr->next;
 	}
