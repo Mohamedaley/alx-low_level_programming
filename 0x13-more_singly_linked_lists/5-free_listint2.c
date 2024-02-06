8b00b729ff2702e5295f9a23320bc1a8ac17232c
vulturepremier <vulturepremier@gmail.com>
2024-02-06 18:49:10 +0200

    My first commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - A function to free a list and sets the *head to NULL
+ * @head: the head pointer of our list;
+ * Return: VOID (nothing)
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = *head;
+
+	while (temp)
+	{
+		temp = temp->next;
+		free(*head);
+		*head = temp;
+	}
+}
