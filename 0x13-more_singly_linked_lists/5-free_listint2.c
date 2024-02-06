3a20199911151ee2aad8c346606188da73a93d2e
vulturepremier <vulturepremier@gmail.com>
2024-02-06 19:08:42 +0200

    My first commit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,9 +11,9 @@ void free_listint2(listint_t **head)
 	if (head == NULL)
 		return;
 
-	while (temp)
+	while (*head)
 	{
-		temp = temp->next;
+		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
