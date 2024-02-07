f2ac138c4417cbcac4c3775c179708a890b85871
vulturepremier <vulturepremier@gmail.com>
2024-02-07 09:37:02 +0200

    My first commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,50 +1,45 @@
 #include "lists.h"
-/**
- * insert_nodeint_at_index - A function to insert nodes at specific position;
- * @head: the head pointer;
- * @idx:the index of the list where the new node should be added,starts at 0
- * @n: the data to be stored in the created node;
- * Return: the address of the new node of NULL if it failed;
- */
 
+/**
+ * insert_nodeint_at_index - inserts a new node in a linked list,
+ * at a given position
+ * @head: pointer to the first node in the list
+ * @idx: index where the new node is added
+ * @n: data to insert in the new node
+ *
+ * Return: pointer to the new node, or NULL
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int len, i;
-	listint_t *ptr, *temp, *linker;
+	unsigned int i;
+	listint_t *new;
+	listint_t *temp = *head;
 
-	linker = *head;
-	ptr = *head;
-	temp = malloc(sizeof(listint_t));
-	if (!temp)
+	new = malloc(sizeof(listint_t));
+	if (!new || !head)
 		return (NULL);
 
-	temp->next = NULL;
-	temp->n = n;
-	if (!(*head))
+	new->n = n;
+	new->next = NULL;
+
+	if (idx == 0)
 	{
-		*head = temp;
-		return (temp);
+		new->next = *head;
+		*head = new;
+		return (new);
 	}
 
-	if (!idx)
+	for (i = 0;i < idx; i++)
 	{
-		temp->next = ptr;
-		*head = temp;
-		return (temp);
+		if (i == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
 	}
-	while (ptr)
-	{
-		ptr = ptr->next;
-		len++;
-	}
-	if (idx > len)
-		return (NULL);
-	ptr = *head;
-	for (i = 0; i < idx; i++)
-		ptr = ptr->next;
-	for (i = 0; i < (idx - 1); i++)
-		linker = linker->next;
-	linker->next = temp;
-	temp->next = ptr;
-	return (temp);
+
+	return (NULL);
 }
