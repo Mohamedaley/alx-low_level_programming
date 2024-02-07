14714236c15b376b2ed8cea9569cbba734ab9022
vulturepremier <vulturepremier@gmail.com>
2024-02-07 09:22:34 +0200

    My first commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	if (!idx)
 	{
-		ptr->next = *head;
+		temp->next = ptr;
 		*head = temp;
 		return (temp);
 	}
