0263e92ebaf96e19f1f998f3c143bb29d473d767
vulturepremier <vulturepremier@gmail.com>
2024-02-15 15:36:44 +0200

    My first commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,47 +1,50 @@
 #include "main.h"
 /**
- * main - A funcion to copy text to new file
- * @argc: the number of arguments
- * @argv: the argument passed
- * Return: differnt values;
+ * main - copies the content of a file to another file
+ * @argc: number of arguments passed to the program
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	void *buffer;
-	ssize_t fd, wr, re;
-	char *fto, *from;
-	int in;
-
-	from = argv[1];
-	fto = argv[2];
-	buffer = malloc(1024);
+	int fd_r, fd_w, x, m, n;
+	char buf{BUFSIZ};
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd = open(from, O_RDONLY);
-	re = read(fd, buffer, 1024);
-	if (fd == -1 || re == -1)
+	fd_r = open(argv[1], O_RDONLY);
+	if (fd_r < 0)
 	{
-		dprintf(2, "Error: Can't read from file %s", from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	close(fd);
-	fd = open(fto, O_RDWR | O_CREAT | O_TRUNC, 0664);
-	wr = write(fd, buffer, 1024);
-	if (fd == -1 || wr == -1)
+	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((x = read(fd_r, buf, BUFSIZ)) > 0)
 	{
-		dprintf(2, "Error: Can't write to %s", fto);
-		exit(99);
+		if (fd_w < 0 || write(fd_w, buf, x) != x)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fd_r);
+			exit(99);
+		}
 	}
-	in = close(fd);
-	free(buffer);
-	if (in == -1)
+	if (x < 0)
 	{
-		dprintf(2, "Error: Can't close fd %ld\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	m = close(fd_r);
+	n = close(fd_w);
+	if (m < 0 || n < 0)
+	{
+		if (m < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
+		if (n < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
 		exit(100);
 	}
-	return (1);
+	return (0);
 }
