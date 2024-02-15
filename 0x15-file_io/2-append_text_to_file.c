5c367b5b5a0bea6ddfca071c7d2b5ae7abdbc6b7
vulturepremier <vulturepremier@gmail.com>
2024-02-15 14:07:30 +0200

    My first commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content != NULL)
+	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
