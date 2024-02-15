aaa9dbe2020b3851c0dfd7510fcda84a5b0b05e1
vulturepremier <vulturepremier@gmail.com>
2024-02-15 13:50:27 +0200

    My first commit

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * append_text_to_file - A function to add text at the end of file
+ * @filename: the name of the file
+ * @text_content: the text should be added
+ * Return: 1 on Success 0 on Fail
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int len;
+	ssize_t fd, wr;
+
+	if (filename == NULL)
+		return (-1);
+	while (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	if (text_content != NULL)
+	{
+		fd = open(filename, O_RDWR | O_APPEND);
+		wr = write(fd, text_content, len);
+	}
+	if (wr == -1 || fd == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
