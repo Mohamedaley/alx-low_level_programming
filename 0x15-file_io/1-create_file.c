ab4a5d8554d992643ba52a29d1ea22e4186e63a6
vulturepremier <vulturepremier@gmail.com>
2024-02-15 03:01:54 +0200

    My first commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,16 +12,19 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
 	}
+
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wr = write(fd, buffer, len);
+	wr = write(fd, text_content, len);
+
 	if (wr == -1 | fd == -1)
 		return (-1);
+
 	close(fd);
 	return (1);
-
 }
