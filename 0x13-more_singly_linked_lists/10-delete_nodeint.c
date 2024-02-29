760d7fe00c9d7dc135b9841508a72b8aebcc2d2a
vulturepremier <vulturepremier@gmail.com>
2024-02-29 16:26:29 +0200

    My first commit

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - A function to delete node at specific index;
+ * @head: the head pointer;
+ * @index: the number of node to be deleted;
+ * Return: 1 if (sccess) -1 if (fail);
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+}
