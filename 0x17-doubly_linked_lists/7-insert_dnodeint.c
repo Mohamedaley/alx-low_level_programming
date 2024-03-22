a9bdaceaca6dac6bfc0b81aeae255207822a51f0
vulturepremier <vulturepremier@gmail.com>
2024-03-22 10:27:17 +0200

    New commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,7 +8,7 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int len = 0, index = 1;
+	unsigned int len = 0, index = 0;
 	dlistint_t *new, *ptr = *h, *next;
 
 	if (h == NULL)
