558afb1d431803a6014f5d808b3b253f0b559df1
vulturepremier <vulturepremier@gmail.com>
2024-03-22 09:14:10 +0200

    New commti

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,7 +8,7 @@
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new = malloc(sizeof(dlistint_t));
-	dlistint_t *ptr = head;
+	dlistint_t *ptr = *head;
 
 	if (new == NULL)
 		return (NULL);
@@ -17,7 +17,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new->n = n;
 	if (*head == NULL)
 	{
-		head = new;
+		*head = new;
 		return (new);
 	}
 	while (ptr->next != NULL)
