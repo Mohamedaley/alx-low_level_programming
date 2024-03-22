3b51c9aded0c17e6e9a9098a64a32de9be9fd60c
vulturepremier <vulturepremier@gmail.com>
2024-03-22 08:41:09 +0200

    New commit

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * dlistint_len - A function to return the number of nodes in the list;
+ * @h: the head ponter;
+ * Return: the number of nodes in the list;
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	dlistint_t *temp = (dlistint_t *)h;
+	size_t count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (temp->prev != NULL)
+	{
+		temp = temp->prev;
+	}
+	while (temp->next)
+	{
+		count++;
+		temp = temp->next;
+	}
+	return (count);
+}
