e988b466336a881d6e69d96e18b6ece28b6be7b0
vulturepremier <vulturepremier@gmail.com>
2024-03-22 09:42:12 +0200

    New commit

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - A function to add a node in specific index;
+ * @head: the head pointer;
+ * @index: a number represeent the index of new node;
+ * Return: the address of index node , NULL if Fail;
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *ptr;
+	int count = 0;
+
+	if (head == NULL)
+		return (NULL);
+	ptr = head;
+	while (count < index)
+	{
+		ptr = ptr->next;
+		count++;
+	}
+	return (ptr);
+}
