53b05ab27473a11b1f307eea7e345bb0a8aed7e7
vulturepremier <vulturepremier@gmail.com>
2024-03-22 09:04:33 +0200

    New commit

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,13 +10,17 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *temp = malloc(sizeof(dlistint_t));
 	dlistint_t *ptr = *head;
 
-	if (*head == NULL)
-		return (NULL);
 	if (temp == NULL)
 		return (NULL);
+
 	temp->next = NULL;
 	temp->n = n;
 	temp->prev = NULL;
+	if (*head == NULL)
+	{
+		*head = temp;
+		return (temp);
+	}
 	temp->next = ptr;
 	ptr->prev = temp;
 	*head = temp;
