0803b2c547d487da8e78a1dd5a1a061d59304e97
Mohamedaley <vulturepremier@gmail.com>
2024-07-16 16:09:30 +0300

    New commit

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,9 @@
 #ifndef SORTING_ALGOS_H
 #define SORTING_ALGOS_H
+
+#include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
